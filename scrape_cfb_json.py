778aba075961b21f7f6345547ee5c4b0b35040aa
saiemgilani <saiem.gilani@gmail.com>
2022-07-19 04:35:49 -0400

    update schedules/scrape_cfb_json

--- scrape_cfb_json.py
+++ scrape_cfb_json.py
@@ -1,32 +1,36 @@
 import os, json
 import re
 import http
+import pandas as pd
+import numpy as np
+import pyarrow.parquet as pq
+import sportsdataverse as sdv
 import xgboost as xgb
 import time
 import urllib.request
 from urllib.error import URLError, HTTPError, ContentTooShortError
 from datetime import datetime
 from itertools import chain, starmap
-import pandas as pd
-import numpy as np
 from pathlib import Path
 from play_handler import PlayProcess, process_cfb_raw_for_gop
 path_to_raw = "pbp_json_raw"
 path_to_final = "pbp_json_final"
 def main():
-    years_arr = range(2021,2022)
-    schedule = pd.read_csv('cfb_schedule_2002_2021.csv', encoding='latin-1')
-    schedule = schedule.sort_values(by=['season', 'week'], ascending = False)
-    schedule = schedule[schedule['status.type.completed']==True]
-
+    years_arr = range(2002,2021)
+    schedule = pd.read_parquet('cfb_schedule_master.parquet', engine='auto', columns=None)
+    schedule["game_id"] = schedule["game_id"].astype(int)
+    print(schedule.columns)
+    schedule = schedule[schedule['status_type_completed']==True]
+    print(schedule.columns)
+    schedule = schedule.sort_values(by=['season','season_type'], ascending = False)
     for year in years_arr:
         print(year)
         games = schedule[(schedule['season']==year)].reset_index()['game_id']
-        print(f"Number of Games: {len(games)}, first gameId: {games[0]}")
+        print(f"Number of Games: {len(games)}, first gameId: {games}")
 
         # this finds our json files
-        path_to_raw_json = "{}/{}/".format(path_to_raw, year)
-        path_to_final_json = "{}/{}/".format(path_to_final, year)
+        path_to_raw_json = "{}/".format(path_to_raw)
+        path_to_final_json = "{}/".format(path_to_final)
         Path(path_to_raw_json).mkdir(parents=True, exist_ok=True)
         Path(path_to_final_json).mkdir(parents=True, exist_ok=True)
         # json_files = [pos_json.replace('.json', '') for pos_json in os.listdir(path_to_raw_json) if pos_json.endswith('.json')]
@@ -41,12 +45,12 @@ def main():
                 i+=1
                 continue
             try:
-                processed_data= PlayProcess( gameId = game, path_to_json = path_to_raw_json)
-                g = processed_data.cfb_pbp()
+                g = sdv.cfb.CFBPlayProcess(gameId = game, raw=True).espn_cfb_pbp()
+
 
             except (TypeError) as e:
                 print("TypeError: yo", e)
-                g = processed_data.cfb_pbp()
+                g = sdv.cfb.CFBPlayProcess(gameId = game, raw=True).espn_cfb_pbp()
 
             except (KeyError) as e:
                 print("KeyError: yo", e)
@@ -67,23 +71,23 @@ def main():
             fp = "{}{}.json".format(path_to_raw_json, game)
             with open(fp,'w') as f:
                 json.dump(g, f, indent=0, sort_keys=False)
-        #     time.sleep(1)
-            if processed_data.playByPlaySource != "none":
-                processed_data.run_processing_pipeline()
-                tmp_json = processed_data.plays_json.to_json(orient="records")
-                jsonified_df = json.loads(tmp_json)
-                g['plays'] = jsonified_df
-                print(processed_data.plays_json.index.size)
-                if processed_data.plays_json.index.size > 50:
-                    box = processed_data.create_box_score()
-                else:
-                    box = np.array([]).tolist()
+            #     time.sleep(1)
+            # if processed_data.playByPlaySource != "none":
+            #     processed_data.run_processing_pipeline()
+            #     tmp_json = processed_data.plays_json.to_json(orient="records")
+            #     jsonified_df = json.loads(tmp_json)
+            #     g['plays'] = jsonified_df
+            #     print(processed_data.plays_json.index.size)
+            #     if processed_data.plays_json.index.size > 50:
+            #         box = processed_data.create_box_score()
+            #     else:
+            #         box = np.array([]).tolist()
 
-                result = process_cfb_raw_for_gop(game, g, jsonified_df, box)
-                g["box_score"] = box
-                fp = "{}/{}.json".format(path_to_final, game)
-                with open(fp,'w') as f:
-                    json.dump(g, f, indent=0, sort_keys=False)
+            #     result = process_cfb_raw_for_gop(game, g, jsonified_df, box)
+            #     g["box_score"] = box
+            #     fp = "{}/{}.json".format(path_to_final, game)
+            #     with open(fp,'w') as f:
+            #         json.dump(g, f, indent=0, sort_keys=False)
             i+=1
 
 if __name__ == "__main__":
--- scrape_cfb_schedules.py
+++ scrape_cfb_schedules.py
@@ -1,43 +1,62 @@
 import os, json
 import re
 import http
+import json
 import time
 import urllib.request
+import pyreadr
+import pyarrow as pa
+import pandas as pd
+import sportsdataverse as sdv
 from urllib.error import URLError, HTTPError, ContentTooShortError
 from datetime import datetime
 from itertools import chain, starmap
-import pandas as pd
 from pathlib import Path
-from schedule_handler import ScheduleProcess
-path_to_schedules = "cfb/schedules/csv"
-path_to_json = "cfb/schedules/json"
-final_file_name = "cfb_schedule_2002_2021.csv"
+path_to_schedules = "cfb/schedules"
+final_file_name = "cfb_schedule_master.csv"
+
+def download_schedule(season, path_to_schedules=None):
+    df = sdv.cfb.espn_cfb_calendar(season)
+    df = df[df['season_type'].isin(['2','3'])]
+
+    ev = pd.DataFrame()
+    for index, row in df.iterrows():
+        date_schedule = sdv.cfb.espn_cfb_schedule(dates=season,
+                                                  season_type=row['season_type'],
+                                                  week=row['week'])
+        ev = pd.concat([ev,date_schedule],axis=0, ignore_index=True)
+    ev = ev.drop('competitors', axis=1)
+    ev = ev.drop_duplicates(subset=['game_id'], ignore_index=True)
+    if path_to_schedules is not None:
+        path_to_csv = "{}/{}/".format(path_to_schedules, 'csv')
+        path_to_parquet = "{}/{}/".format(path_to_schedules, 'parquet')
+        path_to_rds = "{}/{}/".format(path_to_schedules, 'rds')
+        path_to_json = "{}/{}/".format(path_to_schedules, 'json')
+        Path(path_to_csv).mkdir(parents=True, exist_ok=True)
+        Path(path_to_parquet).mkdir(parents=True, exist_ok=True)
+        Path(path_to_rds).mkdir(parents=True, exist_ok=True)
+        Path(path_to_json).mkdir(parents=True, exist_ok=True)
+        ev.to_csv(f"{path_to_schedules}/csv/cfb_schedule_{season}.csv", index = False)
+        ev.to_parquet(f"{path_to_schedules}/parquet/cfb_schedule_{season}.parquet", index = False)
+        pyreadr.write_rds(f"{path_to_schedules}/rds/cfb_schedule_{season}.rds", ev)
+        fp = "{}/cfb_schedule_{}.json".format(path_to_json, season)
+        with open(fp,'w') as f:
+            json.dump(ev.to_json(orient='records'), f, indent=0, sort_keys=False)
+    return ev
 def main():
-    years_arr = range(2021,2022)
+
+    years_arr = range(2002,2023)
     schedule_table = pd.DataFrame()
     for year in years_arr:
-        processor = ScheduleProcess(groups='80', dates=year,season_type=2)
-        try:
-            year_schedule = processor.cfb_schedule_year(groups='80', date=year,season_type=2)
-        except (TypeError) as e:
-            year_schedule = processor.cfb_schedule_year(groups='80', date=year,season_type=2)
-        year_schedule['game_id'] = year_schedule['id']
-
-        year_schedule.to_csv(f"{path_to_schedules}/cfb_schedule_{year}.csv", index=False)
-
-        fp = "{}/cfb_schedule_{}.json".format(path_to_json, year)
-        js = json.loads(year_schedule.to_json(orient='records'))
-        with open(fp,'w') as f:
-            json.dump(js, f, indent=0, sort_keys=False)
-        schedule_table = schedule_table.append(year_schedule)
-    csv_files = [pos_csv.replace('.csv', '') for pos_csv in os.listdir(path_to_schedules) if pos_csv.endswith('.csv')]
+        year_schedule = download_schedule(year, path_to_schedules)
+        schedule_table = pd.concat([schedule_table, year_schedule], axis=0)
+    csv_files = [pos_csv.replace('.csv', '') for pos_csv in os.listdir(path_to_schedules+'/csv') if pos_csv.endswith('.csv')]
     glued_data = pd.DataFrame()
     for index, js in enumerate(csv_files):
-        x = pd.read_csv(f"{path_to_schedules}/{js}.csv", low_memory=False)
-        glued_data = pd.concat([glued_data, x], axis=0)
-    glued_data['game_id'] = glued_data['id']
+        x = pd.read_csv(f"{path_to_schedules}/csv/{js}.csv", low_memory=False)
+        glued_data = pd.concat([glued_data,x],axis=0)
     glued_data.to_csv(final_file_name, index=False)
-
+    glued_data.to_parquet(final_file_name.replace('.csv', '.parquet'), index=False)
 
 if __name__ == "__main__":
     main()
